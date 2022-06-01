87817e3400f35eddd59f33878f1b035dfa2b144a
Rishabh Kumar <riskumar7@WKMZT31C4B93.global.publicisgroupe.net>
2022-06-02 02:04:12 +0530

    Added comments and authorId field

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')  //create an express server that runs a graphql API
 const schema = require('./schema/schema')
 
 const app = express()
 
 app.use(
   '/graphql',
-  graphqlHTTP({
+  graphqlHTTP({         //graphql Middleware takes input like schema, graphiql
     schema,
     graphiql: true,
   })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,94 +1,103 @@
-const graphql = require('graphql')
-const _ = require('lodash')
+const graphql = require("graphql");
+const _ = require("lodash");
 
+const books = [
+  {
+    name: "Sapiens",
+    genre: "Informative",
+    id: "1",
+    authorId:"2"
+  },
+  {
+    name: "The Final Earth",
+    genre: "Fantasy",
+    id: "2",
+    authorId:"1"
+  },
+  {
+    name: "Harry Potter",
+    genre: "Fantasy",
+    id: "3",
+    authorId:"4"
+  },
+  {
+    name: "Autobiography of a Yogi",
+    genre: "Spiritual",
+    id: "4",
+    authorId:"3"
+  },
+];
+
+const authors = [
+  {
+    name: "Yuval Noah Harari",
+    age: 44,
+    id: "1",
+  },
+  {
+    name: "Jk Rowling",
+    age: 42,
+    id: "2",
+  },
+  {
+    name: "Paramhansa Yogananda",
+    age: 100,
+    id: "3",
+  },
+];
+console.log(books);
 const {
-  GraphQLObjectType,
+  GraphQLObjectType, //To create object types like Books and Authors etc.
   GraphQLID,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
-} = graphql
-
-const books = [
-  {
-    name: 'Sapiens',
-    genre: 'Informative',
-    id: '1',
-  },
-  {
-    name: 'The Final Earth',
-    genre: 'Fantasy',
-    id: '2',
-  },
-  {
-    name: 'Harry Potter',
-    genre: 'Fantasy',
-    id: '3',
-  },
-  {
-    name: 'Autobiography of a Yogi',
-    genre: 'Spiritual',
-    id: '4',
-  },
-]
-
-const authors = [
-  {
-    name: 'Yuval Noah Harari',
-    age: 44,
-    id: '1',
-  },
-  {
-    name: 'Jk Rowling',
-    age: 42,
-    id: '2',
-  },
-  {
-    name: 'Paramhansa Yogananda',
-    age: 100,
-    id: '3',
-  },
-]
+} = graphql;
 
 const BookType = new GraphQLObjectType({
-  name: 'Book',
+  name: "Book", //name of object
   fields: () => ({
+    //fields that exist on Book, it is a function since all the types can have references to each other correctly.
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
-})
+});
 
 const AuthorType = new GraphQLObjectType({
-  name: 'Author',
+  name: "Author",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
   }),
-})
+});
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  //Query from where we can enter the schema or graph.
+  name: "RootQueryType",
   fields: {
+    //Fields have object that we can use as rootquery
     book: {
+      //Accept three arguments; type, args(any argument to be used), resolve function.
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
+        //Resolvers use can have 4 arguments, parents, args, context, info.
         //code to get data from db/other source.
-        return _.find(books, { id: args.id })
+        return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id })
+        return _.find(authors, { id: args.id });
       },
     },
   },
-})
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
-})
+});
