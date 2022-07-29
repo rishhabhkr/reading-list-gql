f57456430b1fd2680d8341ac99995e60cc2aad63
Rishabh Kumar <riskumar7@WKMZT31C4B93.global.publicisgroupe.net>
2022-07-29 07:27:53 +0530

    connected to the mongoDB database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')  //create an express server that runs a graphql API
 const schema = require('./schema/schema')
+const mongoose = require("mongoose")
 
 const app = express()
+
+//Connect to the monogoDB database
+mongoose.connect('mongodb+srv://rishabh:test123@gql-reading-list.wrge3f7.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connection.once('open', ()=>{
+  console.log("Connected to database");
+})
 
 app.use(
   '/graphql',
